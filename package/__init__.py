3bb213755a096df01da34bfeea456f61cf69e86d
MTamon <mtamon.0044@gmail.com>
2023-01-29 19:37:48 +0900

    initial comit for this python library

---
+++ package/__init__.py
@@ -0,0 +1,10 @@
+# from .<ファイル> import *
+
+__copyright__ = "Copyright (C) 2022 Tamon Mikawa"
+__version__ = "1.0.0"
+__license__ = "MIT License"
+__author__ = "Tamon Mikawa"
+__author_email__ = "mtamon.engineering@gmail.com"
+__url__ = "https://github.com/MTamon/PypeLine.git"
+
+__all__ = ["公開モジュール"]
